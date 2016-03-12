dabc27b5161d2b86523beea21de3c2c8b7b8b004
Michael Haase <darkstar@gernox.de>
2016-03-12 08:33:26 +0100

    Update docker config

--- cluster/debian/reconfDocker.sh
+++ cluster/debian/reconfDocker.sh
@@ -61,7 +61,7 @@ function restart_docker {
 
   echo DOCKER_OPTS=\"${DOCKER_OPTS} -H tcp://127.0.0.1:4243 -H unix:///var/run/docker.sock \
        --bip=${FLANNEL_SUBNET} --mtu=${FLANNEL_MTU}\" > /etc/default/docker
-  sudo service docker restart
+  sudo systemctl restart docker
 }
 
 if [[ $1 == "i" ]]; then
